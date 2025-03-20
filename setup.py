c5eb950a171e8d346a9fa9bd7b501b2493752fda
difesanchez <difesanchedev@gmail.com>
2025-03-20 09:32:56 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miprimerRelease",                                     # Nombre del paquete
+    version="0.1.0",                                            # Versión inicial
+    packages=find_packages(),                                   # Paquetes a incluir
+    description="Un paquete pip simple de saludo",              # Breve descripción
+    author="Diego F Sanchez G difesanchez",                     # Tu nombre
+    author_email="difesanchezdev@gmail.com",                    # Tu correo electrónico
+    url="https://github.com/difesanchezdev/miprimerRelease",    # URL del proyecto
+)
